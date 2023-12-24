d352d1a8213a8322a310a38458a215d807c247cb
girotte-tao <taoware@qq.com>
2023-12-24 12:42:59 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from app import app, db, socketio
 
 if __name__ == '__main__':
-    socketio.run(app, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
 
