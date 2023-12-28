bd3dc0bd164018b1957d0293b42be0af100aacc7
girotte-tao <taoware@qq.com>
2023-12-28 10:41:31 +0800

    set MAX_CONTENT_LENGTH = 50MB

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'mysql://root:kgsJNPaGMkmun1@localhost:3306/safe_lift_test'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+MAX_CONTENT_LENGTH = 50 * 1024 * 1024
